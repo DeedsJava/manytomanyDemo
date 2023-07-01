39768490f4f387c779f0265007ef55e7c71f965e
Sreeni <sreenivasbv89@gmail.com>
2023-07-01 11:41:35 +0530

    First Commit

---
+++ src/main/java/com/aspirants/manytomanyDemo/ManytomanyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aspirants.manytomanyDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManytomanyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManytomanyDemoApplication.class, args);
+	}
+
+}
